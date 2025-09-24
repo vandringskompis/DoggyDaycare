c24e10b75cfcea0ab1692511938975870defe005
camillafalk <ccamilla.falk@gmail.com>
2025-09-24 22:13:36 +0200

    Can search dog name.

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -9,6 +9,7 @@ const Catalog = () => {
     const apiUrl = 'https://api.jsonbin.io/v3/b/68cec815d0ea881f40845308';
 
     const [dogs, setDogs] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
 
@@ -26,37 +27,41 @@ const Catalog = () => {
 
     }, [])
 
+    const filteredDogs = dogs.filter((dog) => dog.name.toLowerCase().includes(search.toLowerCase()));
+
     return (
         <>
-            <div className="catalog">
 
-                <header>
-                    <h2>Our dogs</h2>
-                </header>
-                <main>
+            <header>
+                <h2>Our dogs</h2>
+            </header>
+            <main>
 
-                    <div>
-                        <input type="text" className="search_field" 
-                        placeholder="Search..."/>
-                        <button className="search_button">Search</button>
+                <div>
+                    <input type="text" className="search_field"
+                        placeholder="Search..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)} />
 
-                    </div>
-                    <div className="dog_catalog">
-                        {Array.isArray(dogs) && dogs.map((dog) => (
-                            <div className="dog_card" key={dog.chipNumber}>
-                                <Link to="/Catalog/DogInfo" state={{ dog }} >
-                                    <img className="dog_img" src={dog.img || defaultImg} alt={dog.name}
-                                        onError={(e) => {
-                                            e.target.onerror = null;
-                                            e.target.src = defaultImg
-                                        }} />
-                                </Link>
-                                <p> {dog.name}</p>
-                            </div>
-                        ))}
-                    </div>
-                </main>
-            </div>
+                </div>
+                <div className="dog_catalog">
+                    {filteredDogs.length > 0 ? (filteredDogs.map((dog) => (
+                        <div className="dog_card" key={dog.chipNumber}>
+                            <Link to="/Catalog/DogInfo" state={{ dog }} >
+                                <img className="dog_img" src={dog.img || defaultImg} alt={dog.name}
+                                    onError={(e) => {
+                                        e.target.onerror = null;
+                                        e.target.src = defaultImg
+                                    }} />
+                            </Link>
+                            <p> {dog.name}</p>
+                        </div>
+                    ))
+                ) : ( 
+                    <p className="no_dogs">No dogs found</p>
+                )}
+                </div>
+            </main>
 
         </>
 
